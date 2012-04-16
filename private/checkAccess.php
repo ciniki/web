083001dae924e456e92a2e85e305237b75ce387c
Andrew Rivett <veggiefrog@gmail.com>
2012-04-16 09:05:12 -0400

    Added events modules

---
+++ private/checkAccess.php
@@ -0,0 +1,72 @@
+<?php
+//
+// Description
+// ===========
+// This function will check the user has access to the web module public methods, and 
+// return a list of other modules enabled for the business.
+//
+// Arguments
+// =========
+// business_id: 		The ID of the business the request is for.
+// 
+// Returns
+// =======
+//
+function ciniki_web_checkAccess($ciniki, $business_id, $method) {
+
+	//
+	// Check if the module is turned on for the business
+	// Check the business is active
+	// Get the ruleset for this module
+	//
+	$strsql = "SELECT ruleset "
+		. ", CONCAT_WS('.', ciniki_business_modules.package, ciniki_business_modules.module) AS module_id "
+		. "FROM ciniki_businesses, ciniki_business_modules "
+		. "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND ciniki_businesses.status = 1 "														// Business is active
+		. "AND ciniki_businesses.id = ciniki_business_modules.business_id "
+		. "AND ciniki_business_modules.status = 1 "														// Business is active
+		. "";
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashIDQuery.php');
+	$rc = ciniki_core_dbHashIDQuery($ciniki, $strsql, 'businesses', 'modules', 'module_id');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['modules']) || !isset($rc['modules']['ciniki.artcatalog']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'608', 'msg'=>'Access denied.'));
+	}
+	$modules = $rc['modules'];
+
+	// Sysadmins are allowed full permissions
+	if( ($ciniki['session']['user']['perms'] & 0x01) == 0x01 ) {
+		return array('stat'=>'ok', 'modules'=>$modules);
+	}
+
+	//
+	// Users who are an owner or employee of a business can see the business 
+	//
+	$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
+		. "AND package = 'ciniki' "
+		. "AND (permission_group = 'owners' OR permission_group = 'employees') "
+		. "";
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'user');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	//
+	// If the user has permission, return ok
+	//
+	if( isset($rc['rows']) && isset($rc['rows'][0]) 
+		&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
+		return array('stat'=>'ok', 'modules'=>$modules);
+	}
+
+	//
+	// By default, fail
+	//
+	return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'609', 'msg'=>'Access denied.'));
+}
+?>
