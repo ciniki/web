5ec855bd83b8f8c84a20ef74f0c0e57df966642a
Andrew Rivett <veggiefrog@gmail.com>
2012-07-01 01:01:46 +0000

    Forgot missing files

---
+++ private/updateUserDisplay.php
@@ -0,0 +1,73 @@
+<?php
+//
+// Description
+// -----------
+// This function will lookup the client domain in the database, and return the business id.
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_web_updateUserDisplay($ciniki, $business_id) {
+	//
+	// Load settings from the database
+	//
+	$strsql = "SELECT COUNT(*) AS num_users "
+		. "FROM ciniki_web_settings "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND detail_key LIKE 'page-contact-user-display-flags%' "
+		. "AND detail_value > 0 "
+		. "";
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'web', 'users');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( isset($rc['users']) && $rc['users']['num_users'] > 0 ) {
+		$detail_value = 'yes';
+	} else {
+		$detail_value = 'no';
+	}
+
+	//
+	// Get the current value
+	//
+	$strsql = "SELECT detail_value "
+		. "FROM ciniki_web_settings "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND detail_key = 'page-contact-user-display' "
+		. "";
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'web', 'setting');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( isset($rc['setting']['detail_value']) ) {
+		$current_value = $rc['setting']['detail_value'];
+	} else {
+		$current_value = '';
+	}
+	
+	$field = 'page-contact-user-display';
+	if( $current_value != $detail_value ) {
+		$strsql = "INSERT INTO ciniki_web_settings (business_id, detail_key, detail_value, date_added, last_updated) "
+			. "VALUES ('" . ciniki_core_dbQuote($ciniki, $business_id) . "'"
+			. ", '" . ciniki_core_dbQuote($ciniki, $field) . "' "
+			. ", '" . ciniki_core_dbQuote($ciniki, $detail_value) . "'"
+			. ", UTC_TIMESTAMP(), UTC_TIMESTAMP()) "
+			. "ON DUPLICATE KEY UPDATE detail_value = '" . ciniki_core_dbQuote($ciniki, $detail_value) . "' "
+			. ", last_updated = UTC_TIMESTAMP() "
+			. "";
+		$rc = ciniki_core_dbInsert($ciniki, $strsql, 'web');
+		if( $rc['stat'] != 'ok' ) {
+			ciniki_core_dbTransactionRollback($ciniki, 'web');
+			return $rc;
+		}
+		ciniki_core_dbAddModuleHistory($ciniki, 'web', 'ciniki_web_history', $business_id, 
+			2, 'ciniki_web_settings', $field, 'detail_value', $detail_value);
+	}
+
+	return array('stat'=>'ok');
+}
+?>
