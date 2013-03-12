d807db45daa8140b213e0736a72040f4e2871ce0
Andrew Rivett <veggiefrog@gmail.com>
2013-03-12 02:15:51 -0400

    Removed sign up button from side menu

--- private/generateMasterIndex.php
+++ private/generateMasterIndex.php
@@ -58,14 +58,14 @@ function ciniki_web_generateMasterIndex($ciniki, $settings) {
 	$content .= "<div id='content'>\n"
 		. "<article class='page'>\n"
 		. "";
-	if( isset($settings['page-about-image']) && $settings['page-about-image'] != '' && $settings['page-about-image'] > 0 ) {
+//	if( isset($settings['page-about-image']) && $settings['page-about-image'] != '' && $settings['page-about-image'] > 0 ) {
 		if( isset($settings['page-signup-active']) && $settings['page-signup-active'] == 'yes' ) {
 			$content .= "<aside>"
-				. "<h2>New Customers</h2>"
-				. "<form action='/signup'>"
-				. "<div class='bigsubmit'><input type='submit' class='bigsubmit' name='signup' value='Get Started' /></div>"
-				. "</form>"
-				. "<br/><br/><br/>"
+//				. "<h2>New Customers</h2>"
+//				. "<form action='/signup'>"
+//				. "<div class='bigsubmit'><input type='submit' class='bigsubmit' name='signup' value='Get Started' /></div>"
+//				. "</form>"
+//				. "<br/><br/><br/>"
 				. "<h2>Existing customers</h2>"
 				. "<form action='/manage' method='POST'>"
 				. "<div class='input'>"
@@ -79,7 +79,8 @@ function ciniki_web_generateMasterIndex($ciniki, $settings) {
 				. "<div class='submit'><input type='submit' class='submit' name='signin' value='Sign In' /></div>"
 				. "</form>"
 				. "</aside>";
-		} else {
+//		} else {
+		} elseif( isset($settings['page-about-image']) && $settings['page-about-image'] != '' && $settings['page-about-image'] > 0 ) {
 			ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'getScaledImageURL');
 			$rc = ciniki_web_getScaledImageURL($ciniki, $settings['page-about-image'], 'original', '500', 0);
 			if( $rc['stat'] != 'ok' ) {
@@ -87,7 +88,7 @@ function ciniki_web_generateMasterIndex($ciniki, $settings) {
 			}
 			$content .= "<aside><div class='image borderless'><img title='' alt='About' src='" . $rc['url'] . "' /></div></aside>";
 		}
-	}
+//	}
 
 	$content .= "<div class='entry-content'>\n"
 		. $page_content
