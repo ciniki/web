ff0fe1dd3243faef41f8e80853d31bf89984f79b
Andrew Rivett <veggiefrog@gmail.com>
2013-11-18 08:03:01 -0500

    Change file not not 500 error to 404 error

--- private/generatePageMembers.php
+++ private/generatePageMembers.php
@@ -43,7 +43,7 @@ function ciniki_web_generatePageMembers($ciniki, $settings) {
 		//
 		// If there was an error locating the files, display generic error
 		//
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1055', 'msg'=>'Unable to locate file'));
+		return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'1055', 'msg'=>'The file you requested does not exist.'));
 	}
 
 	//
