b20994177ce9072dde0ee8776a0e3dccdbcc03d1
Andrew Rivett <veggiefrog@gmail.com>
2014-05-15 15:27:11 -0400

    Fixed up URL dectection in processContent

--- private/processContent.php
+++ private/processContent.php
@@ -23,21 +23,29 @@ function ciniki_web_processContent($ciniki, $unprocessed_content, $pclass='') {
 	$processed_content = preg_replace('/<p class=\'[A-Za-z\- ]*\'>(<h[1-6][^\>]*>[^<]+<\/h[1-6]>)<\/p>/', '$1', $processed_content);
 	
 //	$processed_content = preg_replace('/\r/m', '', $processed_content);
-	$processed_content = preg_replace('/\n/m', '<br/>', $processed_content);
 //	$processed_content = preg_replace('/h2><br\/>/m', 'h2>', $processed_content);
 
 	// Create active links for urls specified without a href= infront, or '>' in front of www
 //	$pattern = '#\b([^\"\'])((https?://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#';
+//	$pattern = '#\b(((?<!(href=(\"|\')|.....>))https?://?|(?<!(://|..>))www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#m';
+//	$pattern1 = '#(www\.[a-zA-Z0-9\-\.]+)#m';
+//	$pattern1 = '#\n([^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#m';
 	$pattern = '#\b(((?<!(href=(\"|\')|.....>))https?://?|(?<!(://|..>))www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#';
 	$callback = create_function('$matches', '
 		$display_url = $matches[1];
+		if( isset($matches[2]) && ($matches[2] == "http://" || $matches[2] == "https://") ) {
+			$display_url = substr($display_url, strlen($matches[2]));
+			$display_url = preg_replace("/\\\\/$/", "", $display_url);
+		}
 		$url = preg_replace("/www/", "http://www", $display_url);
 		return sprintf(\'<a href="%s">%s</a>\', $url, $display_url);
 	');
+//	$processed_content = preg_replace_callback($pattern1, $callback, $processed_content);
 	$processed_content = preg_replace_callback($pattern, $callback, $processed_content);
 
 	$processed_content = preg_replace('/((?<!mailto:|=|[a-zA-Z0-9._%+-])([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,64})(?![a-zA-Z]|<\/[aA]>))/', '<a href="mailto:$1">$1</a>', $processed_content);
 
+	$processed_content = preg_replace('/\n/m', '<br/>', $processed_content);
 	//
 	// Check for email addresses that should be linked
 	//
