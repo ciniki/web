e0bcf75154a0e5cc1010ccfe33fdb20d5a91234b
Andrew Rivett <veggiefrog@gmail.com>
2016-02-23 11:06:50 -0500

    Updated block gallery to account for padded images

--- private/processBlockGallery.php
+++ private/processBlockGallery.php
@@ -15,21 +15,54 @@ function ciniki_web_processBlockGallery(&$ciniki, $settings, $business_id, $bloc
 
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'processURL');
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'getScaledImageURL');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'getPaddedImageURL');
 
 	//
 	// Start content and clear to both sides of the page
 	//
 	$content = '<br clear="both"/>';
+    $images = '';
 
 	if( isset($block['title']) && $block['title'] != '' ) {
 		$content .= "<h2 class='entry-title wide'>" . $block['title'] . "</h2>\n";
 	}
-	ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'generatePageGalleryThumbnails');
-	$rc = ciniki_web_generatePageGalleryThumbnails($ciniki, $settings, $block['base_url'], $block['images'], 125);
-	if( $rc['stat'] != 'ok' ) {
-		return $rc;
+
+	foreach($block['images'] as $inum => $img) {
+        
+		// 
+		// Check if image is not specified
+		//
+		if( $img['image_id'] == 0 ) {
+            continue;
+//			$img_url = "/ciniki-web-layouts/default/img/noimage_240.png";
+		} else {
+            if( isset($block['thumbnail_format']) && $block['thumbnail_format'] == 'square-padded' ) {
+                $version = ((isset($block['image_version'])&&$block['image_version']!='')?$block['image_version']:'thumbnail');
+                $rc = ciniki_web_getPaddedImageURL($ciniki, $img['image_id'], 'original', 
+                    ((isset($block['image_width'])&&$block['image_width']!='')?$block['image_width']:'150'), 
+                    ((isset($block['image_height'])&&$block['image_height']!='')?$block['image_height']:'0'),
+                    ((isset($block['thumbnail_padding_color'])&&$block['thumbnail_padding_color']!='')?$block['thumbnail_padding_color']:'#ffffff') 
+                    );
+            } else {
+                $version = ((isset($block['image_version'])&&$block['image_version']!='')?$block['image_version']:'thumbnail');
+                $rc = ciniki_web_getScaledImageURL($ciniki, $img['image_id'], $version, 
+                    ((isset($block['image_width'])&&$block['image_width']!='')?$block['image_width']:'150'), 
+                    ((isset($block['image_height'])&&$block['image_height']!='')?$block['image_height']:'0') 
+                    );
+            }
+            if( $rc['stat'] != 'ok' ) {
+                return $rc;
+            }
+            $images .= "<div class='image-gallery-thumbnail'>"
+                . "<a href='" . $block['base_url'] . "/" . $img['permalink'] . "'>"
+                . "<img title='" . htmlspecialchars(strip_tags($img['title'])) . "' alt='" . htmlspecialchars(strip_tags($img['title'])) . "' src='" . $rc['url'] . "' /></a>"
+                . "</div>";
+        }
 	}
-	$content .= "<div class='image-gallery'>" . $rc['content'] . "</div>";
+
+    if( $images != '' ) {
+        $content .= "<div class='image-gallery'>" . $images . "</div>";
+    }
 
 	return array('stat'=>'ok', 'content'=>$content);
 }
