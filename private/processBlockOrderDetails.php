f30f55a3cea064e0150dd0db4810101184c41b51
Andrew Rivett <veggiefrog@gmail.com>
2017-02-15 13:41:13 -0500

    Added style for order details

--- private/processBlockOrderDetails.php
+++ private/processBlockOrderDetails.php
@@ -92,7 +92,10 @@ function ciniki_web_processBlockOrderDetails(&$ciniki, $settings, $business_id, 
                         . ($item['quantity'] > 1 ? ' ' . $item['quantity_plural'] : ' ' . $item['quantity_single'])
                         . "</td>";
                 }
-                $content .= "<td id='order_item_price_" . $item['id'] . "'>" . $item['price_text'] . "</td>";
+                $content .= "<td id='order_item_price_" . $item['id'] . "'>" . $item['price_text'] 
+                    . ((isset($item['discount_text']) && $item['discount_text'] != '') ? '<span class="discount-text">' . $item['discount_text'] . '</span>' : '')
+                    . ((isset($item['deposit_text']) && $item['deposit_text'] != '') ? '<span class="deposit-text">' . $item['deposit_text'] . '</span>' : '')
+                    . "</td>";
                 $content .= "<td id='order_item_total_" . $item['id'] . "'>" . $item['total_text'] . "</td>";
                 $content .= "</tr>";
             }
