43acfc7627c63c72a8770b5eaa585a6216bd7c53
Andrew Rivett <veggiefrog@gmail.com>
2021-08-22 21:07:47 -0400

    Fixed bug with processPage image check

--- private/processPage.php
+++ private/processPage.php
@@ -38,7 +38,7 @@ function ciniki_web_processPage(&$ciniki, $settings, $base_url, $page, $args) {
 
     $content .= "<div class='entry-content'>";
     if( isset($page['image_id']) && $page['image_id'] != '' && $page['image_id'] != 0 
-        && ($page['flags']&0x4000) == 0     // Show image on page
+        && (!isset($page['flags']) || ($page['flags']&0x4000) == 0)     // Show image on page
         ) {
         ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'private', 'getScaledImageURL');
         $rc = ciniki_web_getScaledImageURL($ciniki, $page['image_id'], 'original', '500', 0);
